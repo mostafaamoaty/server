9ddf4550cc554823e4d3337c3d65e72f58297f3c
Moaty <mostafaamoaty@gmail.com>
2021-02-01 20:24:00 +0200

    added timestamps, removed orders from user model

--- models/User.js
+++ models/User.js
@@ -3,15 +3,17 @@ const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 6 },
-  address: [{type: String, required: true,}],
-  age: { type: String, required: true },
-  gender: {type: String, required: true},
-  orders: [{ type: mongoose.Types.ObjectId, required: true, ref: "Order" }],
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    billing_address: [{ type: String, required: true }],
+    age: { type: String, required: true },
+    gender: { type: String, required: true },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
 
 userSchema.plugin(uniqueValidator); //to insure user is created before or not
 
